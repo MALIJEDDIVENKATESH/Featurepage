e351e942306063ec9f73f0e5b9670aa91a8466b2
MALIJEDDI VENKATESH <malijeddivenkatesh19062001@gmail.com>
2021-07-04 20:29:34 +0530

    updated code

--- index.js
+++ index.js
@@ -19,25 +19,27 @@ tabEl.addEventListener('shown.bs.tab', function (event) {
   tab.classList.remove('active');
 })
 
-prevtab=document.querySelectorAll(`.count a[href="${prevlink}"] img`);
-prevtab.forEach((img)=>{
-  var link=img.getAttribute('src');
+// prevtab=document.querySelectorAll(`.count a[href="${prevlink}"] img`);
+prevtab.forEach((tab)=>{
+  let img=tab.children[0];
+  let link=img.getAttribute('src');
   link=link.replace('W.svg','B.svg');
   img.src=link;
 
 })
 
 
- var currentlink=event.target.getAttribute('href');
- var currenttab=document.querySelectorAll(`.count a[href="${currentlink}"]`);
+ let currentlink=event.target.getAttribute('href');
+ let currenttab=document.querySelectorAll(`.count a[href="${currentlink}"]`);
  currenttab.forEach((tab)=>{
    tab.classList.add('active');
  })
 
 
-  currenttab=document.querySelectorAll(`.count a[href="${currentlink}"] img`);
-  currenttab.forEach((img)=>{
-    var link=img.getAttribute('src');
+  // currenttab=document.querySelectorAll(`.count a[href="${currentlink}"] img`);
+  currenttab.forEach((tab)=>{
+    let img=tab.children[0];
+    let link=img.getAttribute('src');
     link=link.replace('B.svg','W.svg');
     img.src=link;
 
