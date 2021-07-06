6a4f429cf67d89e4bb3d48d08d98f4b971019a1d
MALIJEDDI VENKATESH <malijeddivenkatesh19062001@gmail.com>
2021-07-06 11:52:24 +0530

    icons changed

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-var activetablinks=document.querySelectorAll(".count .nav-link.active img");
+var activetablinks=document.querySelectorAll(".slider .nav-link.active img");
 
 activetablinks.forEach((img)=>{
   var link=img.getAttribute('src');
-  link=link.replace('B.svg','W.svg');
+  link=link.replace('B.png','W.png');
   img.src=link;
 
 });
 
-var tabEls = document.querySelectorAll('.count .nav-link[data-bs-toggle="tab"]');
+var tabEls = document.querySelectorAll('.slider .nav-link[data-bs-toggle="tab"]');
 tabEls.forEach((tabEl)=>{
   
 tabEl.addEventListener('shown.bs.tab', function (event) {
 
 
  var prevlink=event.relatedTarget.getAttribute('href');
- var prevtab=document.querySelectorAll(`.count a[href="${prevlink}"]`);
+ var prevtab=document.querySelectorAll(`.slider a[href="${prevlink}"]`);
  prevtab.forEach((tab)=>{
   tab.classList.remove('active');
 })
 
-// prevtab=document.querySelectorAll(`.count a[href="${prevlink}"] img`);
+// prevtab=document.querySelectorAll(`.slider a[href="${prevlink}"] img`);
 prevtab.forEach((tab)=>{
   let img=tab.children[0];
   let link=img.getAttribute('src');
-  link=link.replace('W.svg','B.svg');
+  link=link.replace('W.png','B.png');
   img.src=link;
 
 })
 
 
  let currentlink=event.target.getAttribute('href');
- let currenttab=document.querySelectorAll(`.count a[href="${currentlink}"]`);
+ let currenttab=document.querySelectorAll(`.slider a[href="${currentlink}"]`);
  currenttab.forEach((tab)=>{
    tab.classList.add('active');
  })
 
 
-  // currenttab=document.querySelectorAll(`.count a[href="${currentlink}"] img`);
+  // currenttab=document.querySelectorAll(`.slider a[href="${currentlink}"] img`);
   currenttab.forEach((tab)=>{
     let img=tab.children[0];
     let link=img.getAttribute('src');
-    link=link.replace('B.svg','W.svg');
+    link=link.replace('B.png','W.png');
     img.src=link;
 
   })
